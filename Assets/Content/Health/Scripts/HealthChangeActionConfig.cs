3b334da8a3b6f69f2e7efa85104a6135cf522209
Ivan Zhirnov <i.zhirnov@datasakura.com>
2025-11-21 15:37:09 +0700

    config is done

--- Assets/Content/Health/Scripts/HealthChangeActionConfig.cs
+++ Assets/Content/Health/Scripts/HealthChangeActionConfig.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "HealthRemoveItem", menuName = "Configs/Health Remove Item")]
-public class HealthRemoveActionConfig : ShopActionBase
+[CreateAssetMenu(fileName = "HealthChangeAction", menuName = "Configs/Health Change Action")]
+public class HealthChangeActionConfig : ShopActionBase
 {
     public int Value;
 
     public override void Apply(IPlayerData data)
     {
         var health = data.GetData<HealthElementData>();
-        health.Value -= Value;
+        health.Value += Value;
     }
 
     public override bool CanApply(IPlayerData data)
     {
         var health = data.GetData<HealthElementData>();
-        return health.Value >= Value;
+        return health.Value + Value >= 0;
     }
 }
